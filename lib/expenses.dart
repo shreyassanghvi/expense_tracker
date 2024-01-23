fc7538b6742e80f9959f84ab9470f2c412971f40
shreyassanghvi <shreyas.sanghvi11@gmail.com>
2024-01-23 13:14:53 +0530

    init

---
+++ lib/expenses.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Column(
+        children: [
+          Text("chart"),
+          Text("list"),
+        ],
+      ),
+      appBar: AppBar(
+        title: const Text('Expense Tracker'),
+      ),
+    );
+  }
+}
